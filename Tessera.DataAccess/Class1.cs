e0ca16757d5808f08c3d82fc37539ce613e030b2
Ульяна Целых <tselyhkue@gmail.com>
2025-07-28 12:30:48 +0500

    добавила отдельную библиотеку для работы с бд

---
+++ Tessera.DataAccess/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+
+namespace Tessera.DataAccess
+{
+  public class Class1
+  {
+    public IEnumerable<(float[] Vectors, string Name)> GetData()
+    {
+      var connectionString = $"Host=localhost;Port=5432;Username=postgres;Password=111;Database=HaroldBase"; //TODO: найти пароль
+      using var connection = new NpgsqlConnection(connectionString);
+      connection.Open();
+
+      var selectCmd = new NpgsqlCommand("SELECT \"NAME\", \"VECTOR\" FROM \"POLYNOM\".\"ELEMENTDATA\" WHERE \"VECTOR\" IS NOT NULL;", connection);
+      var vertorsAndNames = new List<(float[] Id, string Name)>();
+      using var reader = selectCmd.ExecuteReader();
+      var vectors = (double[])reader[0];
+      var name = reader.GetString(1);
+      yield return (vectors.Select(x => (float)x).ToArray(), name);
+    }
+  }
+}
