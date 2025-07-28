b74285a589b1bd9e22a23d21f1247f314ddae61e
Ульяна Целых <tselyhkue@gmail.com>
2025-07-28 14:19:39 +0500

    смена платформы для wpf

--- Tessera.App/View/MainWindow.xaml.cs
+++ Tessera.App/View/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,20 +8,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Tessera.App.ViewModel;
 
 namespace Tessera.App.View
 {
   /// <summary>
-  /// Логика взаимодействия для MainWindow.xaml
+  /// Interaction logic for MainWindow.xaml
   /// </summary>
   public partial class MainWindow : Window
   {
     public MainWindow()
     {
       InitializeComponent();
-      var viewModel = new MainWindowViewModel();
-      this.DataContext = viewModel;
     }
   }
 }
--- Tessera.App/ViewModel/Base/ViewModelBase.cs
+++ Tessera.App/ViewModel/Base/ViewModelBase.cs
@@ -6,7 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tessera.App.ViewModel.Base
+namespace MappingManager.ViewModel.Base
 {
   public class ViewModelBase : INotifyPropertyChanged
   {
