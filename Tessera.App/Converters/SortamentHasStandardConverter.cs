97ace96aa227f1117f5970f298968a062f98ec8f
Ульяна Целых <tselyhkue@gmail.com>
2025-09-04 14:20:45 +0500

    Добавила проверку для сортаментов на существование стандарта

---
+++ Tessera.App/Converters/SortamentHasStandardConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using Tessera.App.PolinomHandlers.Utils.Constants;
+
+namespace Tessera.App.Converters
+{
+  public class SortamentHasStandardConverter : IValueConverter
+  {
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+      value switch
+      {
+        string sortament when !string.IsNullOrWhiteSpace(sortament)
+            => StandardConstants.Standards.Any(x => sortament.IndexOf(x, StringComparison.OrdinalIgnoreCase) >= 0),
+        _ => true
+      };
+
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+  }
+}
